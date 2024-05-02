8a5fe49ba148e1e09a1baba941b4c41e729be925
410411366 <114377272+410411366@users.noreply.github.com>
2024-05-03 01:26:24 +0800

    Update train+interact未完成版_2.py

--- train+interact未完成版_2.py
+++ train+interact未完成版_2.py
@@ -43,6 +43,27 @@ import torch
 from torch.nn.parallel._functions import Scatter
 from torch.nn.parallel.parallel_apply import parallel_apply
 
+from torch.utils.data import Dataset
+import torch
+
+
+class MyDataset(Dataset):
+    """
+
+    """
+
+    def __init__(self, input_list, max_len):
+        self.input_list = input_list
+        self.max_len = max_len
+
+    def __getitem__(self, index):
+        input_ids = self.input_list[index]
+        input_ids = input_ids[:self.max_len]
+        input_ids = torch.tensor(input_ids, dtype=torch.long)
+        return input_ids
+
+    def __len__(self):
+        return len(self.input_list)
 
 def scatter(inputs, target_gpus, chunk_sizes, dim=0):
     r"""
